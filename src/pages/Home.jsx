b9bf417bfb7189bfbf612661cafd69ea621f084e
Aarya <aaryasrivastava2007@gmail.com>
2025-09-21 15:35:59 +0530

    Hero section created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,56 @@
 import React from "react";
 import './Home.css'
-// const Home=()=>{
-//     return <h1>Welcome to locatr!</h1>
+import { Plus, Search, Upload, Users } from "lucide-react";
 
-
-// }
-// export default Home
-
-
-
-
-//Navbar
-const Navbar = () =>{
+const Home=()=>{
     return(
-        <nav className="navbar">
-            <h1 className="logo">Locatr</h1>
-            <div className="search-bar">
-                <input type="text" placeholder="Search lost items..."/>
-                <button>🔍</button>
+        
+        <div className="hero-section">
+            <h1 className="hero-title">Lost something? Find it here!</h1>
+            <p className="hero-para">Connect with your campus community to find lost items...</p>
+
+
+
+            <div className="hero-buttons">
+            <button className="btn-1">
+                <Plus size={18}/>Post an item</button>
+            <button className="btn-2">
+                <Search size={18}/>Browse items</button>
             </div>
 
-            <div className="nav-actions">
-                <button className="posts">Post Lost items</button>
-                <button className="notifications">🔔</button>
-                <button className="account">👤</button>
+
+
+        <div className="hero-features">
+            <div className="feature-card">
+            <Upload size={28}/>
+            <h3>Post Items</h3>
+            <p>Quickly post lost or found items with photos and descriptions</p>
             </div>
 
-        </nav>
+
+            <div className="feature-card">
+            <Search size={28} />
+            <h3>Easy Search</h3>
+            <p>Filter by category, location, and keywords to find what you need</p>
+            </div>
+
+
+            <div className="feature-card">
+            <Users size={28} />
+            <h3>Get Connected</h3>
+            <p>Direct contact system to reunite items with their owners</p>
+            </div>
+
+
+        </div>
+        </div>
+       
     )
+
+
 }
-export default Navbar
+export default Home
+
+
+
+
